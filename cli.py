804a67947be4656dab92a8a60cae6991dc09dedd
Alexandr Rassadin <arassadin@mera.ru>
2016-02-16 11:01:02 +0300

    pep8 fix

--- cli.py
+++ cli.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import getopt
+import sys
 
-
-
-import getopt,sys
-
-#keep for my env compatibility
+# keep for my env compatibility
 sys.path.append("/home/artsokol/anaconda/lib/python2.7/site-packages")
 
 import pymorphy2
@@ -26,10 +24,10 @@ def get_options(a_period):
         sys.exit()
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h:c", ["help", "corpus="])   
+        opts, args = getopt.getopt(sys.argv[1:], "h:c", ["help", "corpus="])
     except getopt.GetoptError as err:
         # print help information and exit:
-        print(err) # will print something like "option -a not recognized"
+        print(err)  # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
     for o, a in opts:
@@ -43,6 +41,7 @@ def get_options(a_period):
             a_period.append(a.split(','))
         else:
             assert False, "unhandled option"
+
 
 def get_nGramsTemplate(stringToParse):
     return list(stringToParse.upper().split('+'))
@@ -60,37 +59,40 @@ def help():
     print("exit - terminal closing")
     print("help - show this info")
 
+
 def usage():
     print("Usage:")
-    print("cli_tool args") 
-    print("-c first_year,last_year Mandatory parameter. Certain time period shoud be specifyed")
+    print("cli_tool args")
+    print(
+        "-c first_year,last_year Mandatory parameter. Certain time period shoud be specifyed")
     print("-h get usage info")
     print("")
     print("--corpus=year_begin,year_end")
     print("--help the same as -h")
 
 if __name__ == "__main__":
-    input_period=[]
+    input_period = []
     get_options(a_period=input_period)
 
-    #data preparing 
+    # data preparing
     corp = corpus.corpus()
     corp.load('dumps/corp_multy-lemm.dump')
 
-    data = corp.get_lemm(period=[int(input_period[0][0]),int(input_period[0][1])])
+    data = corp.get_lemm(
+        period=[int(input_period[0][0]), int(input_period[0][1])])
     morph = pymorphy2.MorphAnalyzer()
 
     print("Enter n-gramm sequence or help")
     while 1:
         command = input('--> ')
         if command == "help":
-            get_help()
+            help()
         elif command == "exit":
             sys.exit()
         elif command == "":
             continue
         else:
-            #TODO word checking
+            # TODO word checking
             ngram_list = get_nGramsTemplate(command)
             print (ngram_list)
             n_grams = ngrams(data, ngram_list.__len__())
@@ -99,12 +101,19 @@ if __name__ == "__main__":
                 i = 0
 
                 for item in grams:
-                    if (morph.parse(item)[0].tag.POS is None) or \
-                        ((ngram_list[i] != morph.lat2cyr(morph.parse(item)[0].tag.POS)) and not (ngram_list[i] == verb1 and morph.lat2cyr(morph.parse(item)[0].tag.POS) == verb2)):
-                        break #
+                    if (
+                        morph.parse(item)[0].tag.POS is None) or (
+                        (
+                            ngram_list[i] != morph.lat2cyr
+                            (
+                                morph.parse(item)[0].tag.POS
+                            )
+                        ) and not (ngram_list[i] == verb1 and morph.lat2cyr(morph.parse(item)[0].tag.POS) == verb2
+                                   )
+                    ):
+                        break
                     i += 1
-                
-                #print if every item is equal
+
+                # print if every item is equal
                 if i == ngram_list.__len__():
                     print(grams)
-
